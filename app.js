39474f2aad288e7f4e32142cbc347172db3182b7
Adrien Van Lancker <adrienvanlancker@gmail.com>
2023-04-13 16:31:06 +0000

    Added company view

--- app.js
+++ app.js
@@ -1,75 +1,24 @@
-console.log("loading golden layout...");
 const { useState } = React;
 const { WidthProvider, Responsive } = ReactGridLayout;
-
-function removeObjectWithId(arr, id) {
-    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
-  
-    if (objWithIdIndex > -1) {
-      arr.splice(objWithIdIndex, 1);
-    }
-  
-    return arr;
-  }
-
-const ResponsiveGridLayout = WidthProvider(Responsive);
-
-const itemOptions = [
-    { id: 1, title: 'Item 1', width: 2, height: 2, content: <p>"hello world"</p>, selected: false },
-    { id: 2, title: 'Item 2', width: 2, height: 2, selected: false },
-    { id: 3, title: 'Item 3', width: 2, height: 2, selected: false },
-    { id: 4, title: 'Item 4', width: 2, height: 2, selected: false },
-];
-
-var activeItems = []
-
-const Sidebar = ({ addItem, removeItem }) => {
-    // Define an array of item options with a 'selected' property
-
-    const handleSelectItem = (item) => {
-        // Toggle the 'selected' property of the selected item
-        if(!item.selected) {
-            addItem(item);
-            activeItems.push(item);
-        }
-        else {
-            removeItem(item.id);
-            removeObjectWithId(activeItems, item.id);
-            console.log(activeItems)
-        }
-
-        item.selected = !item.selected;
-        // Pass the updated item to the onItemSelect callback
-
-    };
-
-    return (
-        <div id="sidebar" className="vbox-compact">
-            <h2>Add Item:</h2>
-            {itemOptions.map((item, index) => (
-                <div key={index} onClick={() => handleSelectItem(item)}>
-                    <span class="sidebar-item" data-selected={item.selected ? "true" : "false"}>{item.title}</span>
-                </div>
-            ))}
-        </div>
-    );
-};
-
 
 const App = () => {
     // Define initial state with an array of items
     const [items, setItems] = useState([
     ]);
+    const [draggedWindowId, setDraggedWindowId] = useState(null);
 
     // Define a function to add a new item
     const handleAddItem = (item) => {
         setItems([...items, { ...item }]);
+        activeItems.push(item);
+        toggleObjectWithId(itemOptions, item.id);
     };
 
     // Define a function to remove an item by ID
     const handleRemoveItem = (id) => {
         // Filter the state array to remove the item with the matching ID
         setItems(items.filter((item) => item.id !== id));
+        toggleObjectWithId(itemOptions, id);
     };
 
     return (
@@ -83,12 +32,18 @@ const App = () => {
                 cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
                 isResizable={true}
                 verticalCompact={true}
+                draggableHandle=".window-bar"
+                draggableCancel=".window-content"
             >
                 {items.map((item) => (
-                    <div className="window" key={item.id} data-grid={{ w: item.width, h: item.height, x: 0, y: Infinity }} >
-                        <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
-                        <span>{item.title}</span>
-                        {item.content}
+                    <div className="window" key={item.id} data-grid={{ w: item.width, h: item.height, x: 0, y: Infinity }}>
+                        <div className="window-bar hbox">
+                            <span className="window-title">{item.title}</span>
+                            <button className="window-close" onClick={() => handleRemoveItem(item.id)}>X</button>
+                        </div>
+                        <div className="window-content hbox-compact">
+                            {item.content()}
+                        </div>
                     </div>
                 ))}
             </ResponsiveGridLayout>
--- components.js
+++ components.js
@@ -1,5 +1,272 @@
-/* var TestComponent = React.createClass({
-    render: function () {
-        return <h1>{this.props.label}</h1>;
+const { useState, useEffect } = React;
+const { WidthProvider, Responsive } = ReactGridLayout;
+
+function removeObjectWithId(arr, id) {
+    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+
+    if (objWithIdIndex > -1) {
+        arr.splice(objWithIdIndex, 1);
+    }
+
+    return arr;
+}
+
+function toggleObjectWithId(arr, id) {
+    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+
+    arr[objWithIdIndex].selected = !arr[objWithIdIndex].selected;
+
+    return arr;
+}
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const itemOptions = [
+    {
+        id: 1,
+        title: 'Applicants', selected: false,
+        width: 2, height: 2,
+        content: function () { return (<Applicants />) }
     },
-});*/
+    {
+        id: 2,
+        title: 'Companies', selected: false,
+        width: 2, height: 2,
+        content: function () { return (<Companies />) }
+    },
+    {
+        id: 3,
+        title: 'Item 3', selected: false,
+        width: 2, height: 2,
+        content: function () { return <p>"hello world"</p> }
+    },
+    {
+        id: 4,
+        title: 'Item 4', selected: false,
+        width: 2, height: 2,
+        content: function () { return <p>"hello world"</p> }
+    }
+];
+
+var activeItems = []
+
+const Sidebar = function ({ addItem, removeItem }) {
+    // Define an array of item options with a 'selected' property
+
+    const handleSelectItem = (item) => {
+        // Toggle the 'selected' property of the selected item
+        if (!item.selected) {
+            addItem(item);
+        }
+        else {
+            removeItem(item.id);
+            removeObjectWithId(activeItems, item.id);
+            console.log(activeItems)
+        }
+
+        // toggleObjectWithId(itemOptions, item.id);
+    };
+
+    return (
+        <div id="sidebar" className="vbox-compact">
+            <h2>Add Item:</h2>
+            {itemOptions.map((item, index) => (
+                <div key={index} onClick={() => handleSelectItem(item)}>
+                    <span class="sidebar-item" data-selected={item.selected ? "true" : "false"}>{item.title}</span></div>
+            ))}
+        </div>
+    );
+};
+
+const Applicants = function () {
+    // https://ultimatecourses.com/blog/using-async-await-inside-react-use-effect-hook
+    const [applicants, setApplicants] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchApplicants().then((applicants) => {
+            setApplicants(applicants);
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) {
+        return (<div><p>Loading...</p></div>);
+    }
+
+    else if (applicants === undefined) {
+        return (<div><p>Server/Api Error</p></div>);
+    }
+
+    else if (applicants.length === 0) {
+        return (<div><p>Empty</p></div>);
+    }
+
+    return (<div><p>{JSON.stringify(applicants)}</p></div>);
+};
+
+const CompanyTable = function ({ companies, selectedCompany, handleRowClick, handleReloadClick, changeView, handleInputChange, filterText }) {
+    return (
+        <div class="hfill">
+            <div class="hbox-compact hfill dir-center">
+                <input type="search" class="" value={filterText} placeholder="Filter..." onChange={handleInputChange} />
+                <button onClick={handleReloadClick}>Reload</button>
+                <button onClick={handleReloadClick}>New</button>
+                <button onClick={handleReloadClick}>Delete</button>
+                <button onClick={() => { if (selectedCompany !== null) changeView("item") }}>View/Edit</button>
+            </div>
+            <br />
+            <br />
+            <table class="datatable">
+                <thead>
+                    <tr>
+                        <th>Company</th>
+                        <th>Industry</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {companies.map((company, index) => (
+                        <tr onClick={() => handleRowClick(index)} data-selected={selectedCompany == index}>
+                            <td>{company.name}</td>
+                            <td>{company.industry}</td>
+                            <td>{company.description}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                </tfoot>
+            </table>
+        </div>
+    );
+};
+
+const CompanyView = function ({ company, changeView, reloadTable }) {
+    const [viewCompany, setViewCompany] = useState(company);
+    const [loading, setLoading] = useState(false);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setViewCompany(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    const handlePatchClick = () => {
+        setLoading(true);
+        patchCompany(viewCompany).then(() => {
+            setLoading(false);
+            reloadTable();
+            changeView("table");
+        });
+    };
+
+    const handleDeleteClick = () => {
+        setLoading(true);
+        deleteCompany(viewCompany).then(() => {
+            setLoading(false);
+            reloadTable();
+            changeView("table");
+        });
+    };
+
+    return (
+        <div class="hfill">
+            <div class="hbox-compacthandlePostClick-cPostr">
+                <button onClick={() => { reloadTable(); changeView("table") }}>Close</button>
+                <button onClick={handlePatchClick}>Patch</button>
+                <button onClick={handleDeleteClick}>Delete</button>
+            </div>
+            <br /><br />
+            <div class="acc-text">Name:</div>
+            <span class="data-container hfill">{viewCompany.name}</span>
+            <br /><br />
+            <div class="acc-text">Industry:</div>
+            <input name="industry" class="data-container hfill" value={viewCompany.industry} onChange={handleInputChange} />
+            <br /><br />
+            <div class="acc-text">Description:</div>
+            <textarea name="description" class="data-container hfill" value={viewCompany.description} onChange={handleInputChange}>
+            </textarea>
+            <br /><br />
+            <div class="acc-text">Size:</div>
+            <input name="size" type="number" class="data-container hfill" value={viewCompany.size} onChange={handleInputChange} />
+        </div>
+    );
+};
+
+const Companies = function () {
+    // https://ultimatecourses.com/blog/using-async-await-inside-react-use-effect-hook
+    const [companies, setCompanies] = useState([]);
+    const [selectedCompany, setSelectedCompany] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [filterText, setFilterText] = useState("");
+    const [reload, setReload] = useState(false);
+    const [view, setView] = useState("table");
+
+    useEffect(() => {
+        fetchCompanies().then((companies) => {
+            setCompanies(companies);
+            setLoading(false);
+        });
+    }, [reload]);
+
+    const handleInputChange = (event) => {
+        setFilterText(event.target.value);
+    };
+
+
+    const handleRowClick = (index) => {
+        if (index === selectedCompany) setSelectedCompany(null);
+        else setSelectedCompany(index);
+    };
+
+    const handleReloadClick = () => {
+        setLoading(true);
+        setSelectedCompany(null);
+        fetchCompanies({ cache: "no-store" }).then((companies) => {
+            setCompanies(companies);
+            setLoading(false);
+        });
+    };
+
+    const changeView = (view) => {
+        setView(view);
+    };
+
+    if (loading) {
+        return (<div><p>Loading...</p></div>);
+    }
+
+    else if (companies === undefined) {
+        return (<div><p>Server/Api Error</p></div>);
+    }
+
+    else if (companies.length === 0) {
+        return (<div><p>Empty</p></div>);
+    }
+
+    if (view === "table") {
+        return (
+            <CompanyTable
+                companies={companies}
+                selectedCompany={selectedCompany}
+                handleRowClick={handleRowClick}
+                handleReloadClick={handleReloadClick}
+                changeView={changeView}
+                handleInputChange={handleInputChange}
+                filterText={filterText}
+            />
+        );
+    }
+
+    if (view === "item") {
+        return (
+            <CompanyView
+                company={companies[selectedCompany]}
+                changeView={changeView}
+                reloadTable={handleReloadClick}
+            />
+        );
+    }
+}
